1f7a99a4596f7c6de65c7d12b69aaa8c2cd0c30b
Traceless <Traceless0929@outlook.com>
2018-04-28 16:00:12 +0800

    去除无用引用

--- Traceless.TExtension.Tools/ToolClass.cs
+++ Traceless.TExtension.Tools/ToolClass.cs
@@ -5,7 +5,6 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
-using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Text.RegularExpressions;
 using Newtonsoft.Json;
