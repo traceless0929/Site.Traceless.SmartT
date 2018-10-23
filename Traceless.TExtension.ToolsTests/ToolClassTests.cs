4f8fe5b6a213dac4b9f4ca83562ed730e90885d4
Traceless <Traceless0929@outlook.com>
2018-10-23 17:30:04 +0800

    增加测试

---
+++ Traceless.TExtension.ToolsTests/ToolClassTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Traceless.TExtension.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traceless.TExtension.Tools.Tests
+{
+    [TestClass()]
+    public class ToolClassTests
+    {
+        [TestMethod()]
+        public void GetIniTest()
+        {
+            var list = Traceless.TExtension.Tools.ToolClass.GetIni(@"D:\426187673.ini");
+        }
+    }
+}
