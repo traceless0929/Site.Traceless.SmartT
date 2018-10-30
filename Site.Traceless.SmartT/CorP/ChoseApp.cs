fb3090b0353ec9fe91a86d9b1381e4c69d96db5d
Traceless <Traceless0929@outlook.com>
2018-10-30 09:48:46 +0800

    去除警告

--- Site.Traceless.SmartT/CorP/ChoseApp.cs
+++ Site.Traceless.SmartT/CorP/ChoseApp.cs
@@ -11,56 +11,56 @@ using Traceless.TExtension.Tools;
 
 namespace Site.Traceless.SmartT.CorP
 {
-    internal class ChoseApp : Approver
+  internal class ChoseApp : Approver
+  {
+    private readonly IMahuaApi _mahuaApi;
+    public ChoseApp(IMahuaApi mahuaApi, Approver approver)
     {
-        private readonly IMahuaApi _mahuaApi;
-        public ChoseApp(IMahuaApi mahuaApi, Approver approver)
-        {
-            _mahuaApi = mahuaApi;
-            this.SetSuccesser(approver);
-        }
-        public override void ProcessRequset(GroupMessageReceivedContext msg, AnalysisMsg nowModel)
-        {
-            if (Config.ConfigModel.IsFuncOpen("抽锦鲤"))
-            {
-                if (nowModel.What == "抽锦鲤")
-                {
-                    try
-                    {
-                        var data = ToolClass.GetIni(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
-                            $"{msg.FromGroup}.ini"));
-                        if (data == null)
-                        {
-                            _mahuaApi.SendGroupMessage(msg.FromGroup).At(msg.FromQq)
-                                .Text($"群成员未导入，请联系管理员").Done();
-                        }
-                        else
-                        {
-                            var allMember = data.ToList();
-                            var str = nowModel.Who;
-                            var orderid = Guid.NewGuid().ToString("N").Substring(0,5);
-                            _mahuaApi.SendGroupMessage(msg.FromGroup).At(msg.FromQq)
-                                .Text($"5S后,开始从{allMember.Count()}人中抽取幸运锦鲤{str}!").Newline().Text($"锦鲤编号:{orderid}").Done();
-                            System.Threading.Thread.Sleep(5000);
-                            _mahuaApi.SendGroupMessage(msg.FromGroup).Text($"Boom!{orderid}号{str}的锦鲤为！").Newline()
-                                .At(allMember[ToolClass.RandomGet(0, allMember.Count())]).Newline()
-                                .Text("围殴他ヽ(●-`Д´-)ノ！").Done();
-                        }
-
-                    }
-                    catch (Exception ex)
-                    {
-                        successor.ProcessRequset(msg, nowModel);
-                    }
-
-                }
-            }
-            successor.ProcessRequset(msg, nowModel);
-        }
-
-        public override void ProcessRequset(PrivateMessageFromFriendReceivedContext msg, AnalysisMsg nowModel)
-        {
-            successor.ProcessRequset(msg, nowModel);
-        }
+      _mahuaApi = mahuaApi;
+      this.SetSuccesser(approver);
     }
+    public override void ProcessRequset(GroupMessageReceivedContext msg, AnalysisMsg nowModel)
+    {
+      if (Config.ConfigModel.IsFuncOpen("抽锦鲤"))
+      {
+        if (nowModel.What == "抽锦鲤")
+        {
+          try
+          {
+            var data = ToolClass.GetIni(Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
+                $"{msg.FromGroup}.ini"));
+            if (data == null)
+            {
+              _mahuaApi.SendGroupMessage(msg.FromGroup).At(msg.FromQq)
+                  .Text($"群成员未导入，请联系管理员").Done();
+            }
+            else
+            {
+              var allMember = data.ToList();
+              var str = nowModel.Who;
+              var orderid = Guid.NewGuid().ToString("N").Substring(0, 5);
+              _mahuaApi.SendGroupMessage(msg.FromGroup).At(msg.FromQq)
+                  .Text($"5S后,开始从{allMember.Count()}人中抽取幸运锦鲤{str}!").Newline().Text($"锦鲤编号:{orderid}").Done();
+              System.Threading.Thread.Sleep(5000);
+              _mahuaApi.SendGroupMessage(msg.FromGroup).Text($"Boom!{orderid}号{str}的锦鲤为！").Newline()
+                  .At(allMember[ToolClass.RandomGet(0, allMember.Count())]).Newline()
+                  .Text("围殴他ヽ(●-`Д´-)ノ！").Done();
+            }
+
+          }
+          catch
+          {
+            successor.ProcessRequset(msg, nowModel);
+          }
+
+        }
+      }
+      successor.ProcessRequset(msg, nowModel);
+    }
+
+    public override void ProcessRequset(PrivateMessageFromFriendReceivedContext msg, AnalysisMsg nowModel)
+    {
+      successor.ProcessRequset(msg, nowModel);
+    }
+  }
 }
--- Traceless.TExtension.Tools/CQImageTool.cs
+++ Traceless.TExtension.Tools/CQImageTool.cs
@@ -6,44 +6,44 @@ using System.Threading.Tasks;
 
 namespace Traceless.TExtension.Tools
 {
-    public class CQImageTool
+  public class CQImageTool
+  {
+    /// <summary>
+    /// 通过CQ图片码获取图片HTTP地址
+    /// </summary>
+    /// <param name="imgCode"></param>
+    /// <returns></returns>
+    public static string GetCqImgCodeRawUrl(string imgCode)
     {
-        /// <summary>
-        /// 通过CQ图片码获取图片HTTP地址
-        /// </summary>
-        /// <param name="imgCode"></param>
-        /// <returns></returns>
-        public static string GetCqImgCodeRawUrl(string imgCode)
-        {
-            try
-            {
-                var str = imgCode.Substring(15, imgCode.Length-16);
-                return GetCqImgRawUrl(str);
-            }
-            catch(Exception ex)
-            {
-                return null;
-            }
-        }
-
-        /// <summary>
-        /// 通过CQIMG文件获取图片HTTP地址
-        /// </summary>
-        /// <param name="fileName"></param>
-        /// <returns></returns>
-        public static string GetCqImgRawUrl(string fileName)
-        {
-            try
-            {
-                var ret = ToolClass.fileToString(@"\data\image\" + fileName + ".cqimg");
-                var res = ret.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
-                var str = res.Where(p => p.StartsWith("url=")).Select(p => p.Substring(4)).FirstOrDefault();
-                return str;
-            }
-            catch
-            {
-                return null;
-            }
-        }
+      try
+      {
+        var str = imgCode.Substring(15, imgCode.Length - 16);
+        return GetCqImgRawUrl(str);
+      }
+      catch
+      {
+        return null;
+      }
     }
+
+    /// <summary>
+    /// 通过CQIMG文件获取图片HTTP地址
+    /// </summary>
+    /// <param name="fileName"></param>
+    /// <returns></returns>
+    public static string GetCqImgRawUrl(string fileName)
+    {
+      try
+      {
+        var ret = ToolClass.fileToString(@"\data\image\" + fileName + ".cqimg");
+        var res = ret.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+        var str = res.Where(p => p.StartsWith("url=")).Select(p => p.Substring(4)).FirstOrDefault();
+        return str;
+      }
+      catch
+      {
+        return null;
+      }
+    }
+  }
 }
